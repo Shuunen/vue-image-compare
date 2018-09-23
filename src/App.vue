fc3356abd20c7bf997c6145fd42a0b5dc80b288a
Muah <ctf0@users.noreply.github.com>
2018-09-24 01:32:29 +0200

    add options

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,61 @@
 <template>
-  <div id="app">
-    <ImageCompare before="assets/before.jpg" after="assets/after.jpg" full isZoomable isSwitchable isDraggable showHandleOnDrag>
-      <i class="fa fa-angle-left" aria-hidden="true" slot="icon-left"></i>
-      <i class="fa fa-angle-right" aria-hidden="true" slot="icon-right"></i>
-    </ImageCompare>
-  </div>
+    <div id="app">
+        <ImageCompare :before="before" :after="after">
+            <i slot="icon-left" class="fa fa-angle-left" aria-hidden="true"/>
+            <i slot="icon-right" class="fa fa-angle-right" aria-hidden="true"/>
+        </ImageCompare>
+
+        <hr>
+        <label>Before :</label>
+        <input v-model="before">
+        <label>After :</label>
+        <input v-model="after">
+        <br>
+        <input v-model="full" type="checkbox">
+        <input v-model="hideAfter" type="checkbox">
+        <input v-model="isZoomable" type="checkbox">
+        <input v-model="isSwitchable" type="checkbox">
+        <input v-model="isDraggable" type="checkbox">
+        <input v-model="hideHandle" type="checkbox">
+        <br>
+        <label>Labels :</label>
+        <input v-model="labels.after">
+        <input v-model="labels.before">
+
+        <br>
+        <label>Zoom :</label>
+        <input v-model.number="zoom.min">
+        <input v-model.number="zoom.max">
+    </div>
 </template>
 
 <script>
 import ImageCompare from 'vue-image-compare2'
 
 export default {
-  name: 'app',
   components: {
-    ImageCompare
+      ImageCompare
+  },
+  name: 'app',
+  data() {
+    return {
+      before: 'assets/before.jpg',
+      after: 'assets/after.jpg',
+      full: true,
+      hideAfter: false,
+      isZoomable: true,
+      isSwitchable: true,
+      isDraggable: true,
+      hideHandle: false,
+      labels: {
+        before: '',
+        after: ''
+      },
+      zoom: {
+        min: '',
+        max: ''
+      }
+    }
   }
 }
 </script>
