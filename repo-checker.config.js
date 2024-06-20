048cb1f44739756cfe85336d2d7f89ac3ac33615
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-06-20 20:49:49 +0200

    bump repo checker cfg

--- repo-checker.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable camelcase */
-module.exports = {
-  user_name: 'Romain Racamier-Lafon',
-  user_mail: 'romain.racamier@gmail.com',
-  license: 'MIT',
-  max_size_ko: 12,
-  npm_package: true,
-}
-/* eslint-enable camelcase */
